54cc29e878c7f5bd710b2a17de0b1801a30d97cb
l-Il <66788603+l-Il@users.noreply.github.com>
2021-05-03 23:40:54 +0300

    Third commit

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500
-const MAX_SPEED = 60
-const ROLL_SPEED = 75
-const FRICTION = 500
+export var ACCELERATION = 500
+export var MAX_SPEED = 60
+export var ROLL_SPEED = 75
+export var FRICTION = 500
 
 enum {
 	MOVE,
