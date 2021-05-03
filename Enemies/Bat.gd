94655f4e4abd3fe669aceca516bc5c52e3ea74b1
l-Il <66788603+l-Il@users.noreply.github.com>
2021-05-03 23:31:42 +0300

    Third commit

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,10 +1,40 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
+enum {
+	IDLE,
+	WONDER,
+	CHASE
+}
+
 var knockback = Vector2.ZERO
+
+var state = IDLE
+
+onready var stats = $Stats
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, delta * 200)
 	knockback = move_and_slide(knockback)
 
+	match state:
+		IDLE:
+			pass
+			
+		WONDER:
+			pass
+			
+		CHASE:
+			pass
+
 func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+
+func _on_Stats_no_health():
+	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
+
