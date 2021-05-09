2cb3caaf715170965073f9b7f14bb922af55ae02
l-Il <66788603+l-Il@users.noreply.github.com>
2021-05-09 23:49:23 +0300

    Final commit

--- Hitboxes and Hurtboxes/Hurtbox.gd
+++ Hitboxes and Hurtboxes/Hurtbox.gd
@@ -8,6 +8,7 @@ signal invincibility_started
 signal invincibility_ended
 
 onready var timer = $Timer
+onready var collisionShape = $CollisionShape2D
 
 func set_invincible(value):
 	invincible = value
@@ -30,7 +31,7 @@ func _on_Timer_timeout():
 	self.invincible = false
 
 func _on_Hurtbox_invincibility_started():
-	set_deferred("monitorable", false)
+	collisionShape.set_deferred("disabled", true)
 
 func _on_Hurtbox_invincibility_ended():
-	monitorable = true
+	collisionShape.set_deferred("disabled", false)
